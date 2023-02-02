7d435634d30c68ddd371018502188e11d10ba62b
Mithuncy <58121761+Mithuncy01@users.noreply.github.com>
2023-02-02 22:08:05 +0600

    firebase

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDLSOF8FUVsg8I3iZHkqRZE8anHJg8xiZ0",
-    authDomain: "resturantapp-864c1.firebaseapp.com",
-    databaseURL: "https://resturantapp-864c1-default-rtdb.firebaseio.com",
-    projectId: "resturantapp-864c1",
-    storageBucket: "resturantapp-864c1.appspot.com",
-    messagingSenderId: "1014142855115",
-    appId: "1:1014142855115:web:a3acc5c4716664f62a384e"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
